72176a405309b0e81c82da7da23bea117f952a2f
wenwu <wenwu@bianjie.ai>
2019-11-17 17:32:20 +0800

    first commitgit

---
+++ index.js
@@ -0,0 +1,12 @@
+var Stream = require('node-rtsp-stream');
+
+console.log(Stream);
+// stream = new Stream({
+// 	// name: 'name',
+// 	// // streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
+// 	// wsPort: 9999,
+// 	// ffmpegOptions: { // options ffmpeg flags
+// 	// 	'-stats': '', // an option with no neccessary value uses a blank string
+// 	// 	'-r': 30 // options with required values specify the value after the key
+// 	// }
+// });
