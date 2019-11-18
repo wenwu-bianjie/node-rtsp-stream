d6150e41bad3cf24795fe54fd3fdc1fdf82f2a64
2wangmeisong <2wangmeisong@upoa.com>
2019-11-18 09:20:58 +0800

    change

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var Stream = require('node-rtsp-stream');
 
-console.log(Stream);
-// stream = new Stream({
-// 	// name: 'name',
-// 	// // streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-// 	// wsPort: 9999,
-// 	// ffmpegOptions: { // options ffmpeg flags
-// 	// 	'-stats': '', // an option with no neccessary value uses a blank string
-// 	// 	'-r': 30 // options with required values specify the value after the key
-// 	// }
-// });
+new Stream({
+	name: 'name',
+	streamUrl: 'rtsp://admin:admin@172.18.135.130:554/1/h264major',
+	wsPort: 9999,
+	ffmpegOptions: { // options ffmpeg flags
+		'-stats': '', // an option with no neccessary value uses a blank string
+		'-r': 30 // options with required values specify the value after the key
+	}
+});
